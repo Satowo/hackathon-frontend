b8889ffb9dc943be3a9c09a500bdb6344e4bb428
satowo619 <satodor1001@gmail.com>
2023-06-08 16:31:47 +0900

    add all-channels

--- my-app-ts/src/pages/myPage/components/channelHeader.tsx
+++ my-app-ts/src/pages/myPage/components/channelHeader.tsx
@@ -9,30 +9,30 @@ type Channel = {
 
 type ChannelHeaderProps = {
     logout: () => Promise<void>;
-    channel: Channel | undefined;
+    nowChannel: Channel | undefined;
     getChannelMembers: (channel: Channel | undefined) => Promise<void>;
     getMessages: (channel: Channel | undefined) => Promise<void>;
 };
 
-const ChannelHeader: React.FC<ChannelHeaderProps>= ({logout, channel, getChannelMembers, getMessages}) => {
+const ChannelHeader: React.FC<ChannelHeaderProps>= ({logout, nowChannel, getChannelMembers, getMessages}) => {
     const onClick = () => {
         logout();
     };
 
     const _onClick = () => {
-        getChannelMembers(channel);
+        getChannelMembers(nowChannel);
     };
 
     const _onClick_ = () => {
-        getMessages(channel);
+        getMessages(nowChannel);
     };
 
     return (
         <div className="p-2 bg-purple-900 ">
             <div className="flex items-center ">
-                {(channel === undefined) ? (<p className="w-4/5"></p>) : (
+                {(nowChannel === undefined) ? (<p className="w-4/5"></p>) : (
                 <p className="w-4/5 flex items-center text-gray-300 text-opacity-100 text-xl font-bold ">
-                    {"#  "+channel?.channelName}
+                    {"#  "+nowChannel?.channelName}
                 </p>
                 )}
                 <div className="flex items-center justify-end space-x-6 ">
